2524936d32689c249b902801ec24f80ea08057c4
tuAnnG <tuanng.info@gmail.com>
2023-05-18 12:23:26 +0700

    mobile device

--- static/main.js
+++ static/main.js
@@ -63,4 +63,16 @@ loop = () => {
 
 loop()
 
+let bottomNavItems = document.querySelectorAll('.mb-nav-item')
 
+let bottomMove = document.querySelector('.mb-move-item')
+
+bottomNavItems.forEach((item, index) => {
+    item.onclick = (e) => {
+        console.log('object')
+        let crrItem = document.querySelector('.mb-nav-item.active')
+        crrItem.classList.remove('active')
+        item.classList.add('active')
+        bottomMove.style.left = index * 25 + '%'
+    }
+})
